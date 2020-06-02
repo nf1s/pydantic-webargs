09483285c7737fa40ddda23d299cc8f08bba5a23
ahmednafies <ahmed.nafies@gmail.com>
2020-06-03 01:14:44 +0200

    new release

--- pydantic_webargs/__init__.py
+++ pydantic_webargs/__init__.py
@@ -6,7 +6,7 @@ from pydantic import ValidationError
 __author__ = "Ahmed Nafies Okasha Mohamed <ahmed.nafies@gmail.com>"
 __copyright__ = "Copyright 2020, Ahmed Nafies Okasha Mohamed"
 __license__ = "MIT"
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 
 BODY_METHODS = ["POST", "PUT", "PATCH"]

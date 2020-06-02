bd33ef675636df1591f2f28e251c85a3973149ce
ahmednafies <ahmed.nafies@gmail.com>
2020-06-03 01:13:40 +0200

    fixes decorator

--- pydantic_webargs/__init__.py
+++ pydantic_webargs/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from functools import wraps
-
+from flask import request
 from pydantic import ValidationError
 
 __author__ = "Ahmed Nafies Okasha Mohamed <ahmed.nafies@gmail.com>"
@@ -21,7 +21,7 @@ class InvalidOperation(Error):
     pass
 
 
-def validate(request, query, body):
+def validate(query, body):
 
     payload = None
     query_params = None
@@ -31,13 +31,11 @@ def validate(request, query, body):
             f"Http method '{request.method}' does not contain a payload,"
             "yet a Pyndatic model for body was suppied"
         )
-
     if body:
         payload = body(**request.json).dict()
 
     if query:
         params = request.args
-        params = {k: v[0] for k, v in params.items()}
         query_params = query(**params).dict()
 
     return dict(payload=payload, query=query_params)
@@ -46,13 +44,13 @@ def validate(request, query, body):
 def webargs(query=None, body=None):
     def decorator(f):
         @wraps(f)
-        def decorated_function(request, *args, **kwargs):
+        def decorated_function(*args, **kwargs):
             try:
-                result = validate(request, query, body)
+                result = validate(query, body)
             except ValidationError as e:
                 return str(e)
             kwargs.update(result)
-            response = f(request, *args, **kwargs)
+            response = f(*args, **kwargs)
             return response
 
         return decorated_function

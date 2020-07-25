1d4bb3329ba9b45513b4766f3db6d7a60355c0a4
ahmednafies <ahmed.nafies@gmail.com>
2020-07-25 10:43:58 +0200

    fixes doc links

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     description="Pydatic webargs",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://ahmednafies.github.io/pydantic_webargs/",
+    url="https://ahmednafies.github.io/pydantic-webargs/",
     author="Ahmed Nafies",
     author_email="ahmed.nafies@gmail.com",
     license="MIT",
@@ -35,8 +35,8 @@ setup(
         "docs": ["mkdocs", "mkdocs-material"],
     },
     project_urls={
-        "Documentation": "https://ahmednafies.github.io/pydantic_webargs/",
-        "Source": "https://github.com/ahmednafies/pydantic_webargs",
+        "Documentation": "https://ahmednafies.github.io/pydantic-webargs/",
+        "Source": "https://github.com/ahmednafies/pydantic-webargs",
     },
     classifiers=[
         "Intended Audience :: Developers",
